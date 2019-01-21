1cd3ee0971e2160873e2066450fa047da909d2ad
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-01-21 18:37:23 +0530

    Update Get-DatabricksJobs.tests.ps1

--- Tests/Get-DatabricksJobs.tests.ps1
+++ Tests/Get-DatabricksJobs.tests.ps1
@@ -3,35 +3,5 @@ $BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt" # Create this file 
 $Region = "west europe" 
 $global:jobs 
 
-Describe "Get-DatabricksJobs" {
-    BeforeAll{
-        $JobName = "Test script job4"
-        $SparkVersion = "4.1.x-scala2.11"
-        $NodeType = "Standard_D3_v2"
-        $MinNumberOfWorkers = 2
-        $MaxNumberOfWorkers = 10
-        $Timeout = 1000
-        $MaxRetries = 1
-        $ScheduleCronExpression = "0 15 22 ? * *"
-        $Timezone = "Europe/Warsaw"
-        $NotebookPath = "/Shared/Test"
-        $NotebookParametersJson = '{"key": "value", "name": "test2"}'
-
-        $JobId = Add-DatabricksNotebookJob -BearerToken $BearerToken -Region $Region -JobName $JobName `
-            -SparkVersion $SparkVersion -NodeType $NodeType `
-            -MinNumberOfWorkers $MinNumberOfWorkers -MaxNumberOfWorkers $MaxNumberOfWorkers `
-            -Timeout $Timeout -MaxRetries $MaxRetries `
-            -ScheduleCronExpression $ScheduleCronExpression `
-            -Timezone $Timezone -NotebookPath $NotebookPath `
-            -NotebookParametersJson $NotebookParametersJson 
-    }
-    It "Simple Fetch" {
-        $global:jobs = Get-DatabricksJobs -BearerToken $BearerToken -Region $Region
-        $global:jobs.job_id | Should -BeGreaterThan 0
-    }
-
-    AfterAll{
-        Remove-DatabricksJob -Region $Region -BearerToken $BearerToken -JobId $global:jobs.job_id
-    }
-}
+echo "Hello World" > suvam.txt
 
