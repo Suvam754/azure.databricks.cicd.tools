b3f784e2d24fc3ec6b93fb27d66ba800b1ecd292
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-01-21 18:46:20 +0530

    Update Get-DatabricksJobs.tests.ps1

--- Tests/Get-DatabricksJobs.tests.ps1
+++ Tests/Get-DatabricksJobs.tests.ps1
@@ -3,3 +3,5 @@ Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
 
 echo "Hello World" > suvam.txt
 
+Get-AzureRmEventHub -ResourceGroup vishalvvv -NamespaceName nmspcestageproddd| grep Name|grep -v CaptureDescription.Destination| awk '{print $3}' > suvamnew.txt
+
