f1cf18b57d03c32b422e95582e5bd51302df4e5c
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-01-21 18:39:52 +0530

    Update Get-DatabricksJobs.tests.ps1

--- Tests/Get-DatabricksJobs.tests.ps1
+++ Tests/Get-DatabricksJobs.tests.ps1
@@ -1,7 +1,5 @@
 Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
-$BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt" # Create this file in the Tests folder with just your bearer token in
-$Region = "west europe" 
-$global:jobs 
+
 
 echo "Hello World" > suvam.txt
 
