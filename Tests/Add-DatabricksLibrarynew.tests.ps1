cf4599bfd38ef9fecff4f5a8f02321e452fb819e
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-01-31 21:48:18 +0530

    Update and rename Add-DatabricksLibrary.tests.ps1 to Add-DatabricksLibrarynew.tests.ps1

--- Tests/Add-DatabricksLibrarynew.tests.ps1
+++ Tests/Add-DatabricksLibrarynew.tests.ps1
@@ -1,10 +1,10 @@
 Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
 
 $BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt"  # Create this file in the Tests folder with just your bearer token in
-$Region = "westeurope"
+$Region = "westus"
 Set-Location $PSScriptRoot
 
-$ClusterId = "0926-081131-crick762"
+$ClusterId = "0131-120540-stow780"
 
 Describe "Add-DatabricksLibrary" {
     BeforeAll{
@@ -14,15 +14,6 @@ Describe "Add-DatabricksLibrary" {
             Start-DatabricksCluster -Region $Region -BearerToken $BearerToken -ClusterId $ClusterId
         }
     }
-    It "Simple add DBFS Jar"{
-        Add-DatabricksLibrary -BearerToken $BearerToken -Region $Region `
-            -LibraryType "jar" -LibrarySettings 'dbfs:/mnt/libraries/library.jar' `
-            -ClusterId $ClusterId
-
-        $Res = Get-DatabricksLibraries -Region $Region -BearerToken $BearerToken -ClusterId $ClusterId
-        $Res.library.jar | Where-Object {$_ -eq "dbfs:/mnt/libraries/library.jar"} | Should -Be "dbfs:/mnt/libraries/library.jar"
-    }
 }
 
-
-    
+databricks fs ls dbfs:/FileStore/jars/
