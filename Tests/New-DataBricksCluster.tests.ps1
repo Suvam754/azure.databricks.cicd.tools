282d91c4e0018659c9d0225f567a3045d3c950e1
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-02-04 17:55:34 +0530

    Update New-DataBricksCluster.tests.ps1

--- Tests/New-DataBricksCluster.tests.ps1
+++ Tests/New-DataBricksCluster.tests.ps1
@@ -6,7 +6,6 @@
 )
 Set-Location $PSScriptRoot
 Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
-$BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt" # Create this file in the Tests folder with just your bearer token in
 $Region = "West US" 
 $ClusterName="dbrclstredhqa"
 $SparkVersion="4.0.x-scala2.11"
