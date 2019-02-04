52d7b44a6ae1a432286221f6c293762e00316db0
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-02-04 17:53:32 +0530

    Update New-DataBricksCluster.tests.ps1

--- Tests/New-DataBricksCluster.tests.ps1
+++ Tests/New-DataBricksCluster.tests.ps1
@@ -4,25 +4,27 @@
     [String]$Databrickstokennn
 
 )
+Set-Location $PSScriptRoot
 Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
-echo $Databrickstokennn
-$Region = "westeurope" 
-$ClusterName="vsdsfafasfasf"
+$BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt" # Create this file in the Tests folder with just your bearer token in
+$Region = "West US" 
+$ClusterName="dbrclstredhqa"
 $SparkVersion="4.0.x-scala2.11"
-$NodeType="Standard_D3_v2"
+$NodeType="Standard_DS3_v2"
 $MinNumberOfWorkers=1
 $MaxNumberOfWorkers=1
 $Spark_conf = '{"spark.speculation": true, "spark.streaming.ui.retainedBatches": 5}'
+#$CustomTags = '{"key": "CreatedBy", "value": "suvam"}' , '{"key":"Date","value":"12th Dec 2018"}'
 # $InitScripts = '{"key": "Script1", "value": "dbfs:/script/script1"}', '{"key":"Script2","value":"dbfs:/script/script2"}'
 $SparkEnvVars = '{"key": "SPARK_WORKER_MEMORY", "value": "29000m"}', '{"key":"SPARK_LOCAL_DIRS","value":"/local_disk0"}'
-$AutoTerminationMinutes = 15
+$AutoTerminationMinutes = 10
 $PythonVersion = 2
 
 Describe "New-DatabricksCluster" {
     It "Create basic cluster"{
         $ClusterId = New-DatabricksCluster  -BearerToken $Databrickstokennn -Region $Region -ClusterName $ClusterName -SparkVersion $SparkVersion -NodeType $NodeType `
             -MinNumberOfWorkers $MinNumberOfWorkers -MaxNumberOfWorkers $MaxNumberOfWorkers `
-            -Spark_conf $Spark_conf -AutoTerminationMinutes $AutoTerminationMinutes `
-            -Verbose -SparkEnvVars $SparkEnvVars -PythonVersion $PythonVersion   # -UniqueNames -Update
+            -AutoTerminationMinutes $AutoTerminationMinutes `
+            -Verbose -PythonVersion $PythonVersion   # -UniqueNames -Update
     }
 }
