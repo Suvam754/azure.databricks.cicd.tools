476832053a41d9907ddd5bc7531d08d019e54c68
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-02-08 14:54:16 +0530

    Update Add-DatabricksSecretScope.tests.ps1

--- Tests/Add-DatabricksSecretScope.tests.ps1
+++ Tests/Add-DatabricksSecretScope.tests.ps1
@@ -1,8 +1,8 @@
 Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
 
-$BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt"  # Create this file in the Tests folder with just your bearer token in
-$Region = "westeurope"
-$ScopeName = "DataThirst"
+$BearerToken = "dapi097e322750f9648e47de4f87ae81b8cc"  # Create this file in the Tests folder with just your bearer token in
+$Region = "westus"
+$ScopeName = "Testscopecreation"
 
 Describe "Add-DatabricksSecretScope" {
     It "Simple addition"{
