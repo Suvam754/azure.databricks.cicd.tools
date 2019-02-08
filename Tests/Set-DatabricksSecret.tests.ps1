4a9aaa5e0371e4e9ee674d07e43e9a041eb62e3f
Suvam754 <43569805+Suvam754@users.noreply.github.com>
2019-02-08 14:43:19 +0530

    Update Set-DatabricksSecret.tests.ps1

--- Tests/Set-DatabricksSecret.tests.ps1
+++ Tests/Set-DatabricksSecret.tests.ps1
@@ -2,7 +2,7 @@ Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
 
 $BearerToken = "dapi097e322750f9648e47de4f87ae81b8cc"  # Create this file in the Tests folder with just your bearer token in
 $Region = "westus"
-$ScopeName = "Testscope_creation"
+$ScopeName = "Testscopecreation"
 $SecretName = "databrickstestsec"
 $SecretValue = "testdatabricksvalue"
 
